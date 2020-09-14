4bd84eb82940da7bb0f14db8e39954513fc30fc5
GiDeON gideonov <gideonmaxima@gmail.com>
2020-09-14 07:33:14 +0300

    sign_in, sign_up(#0)

--- main.py
+++ main.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home_page():
     return render_template("index.html")
 
+@app.route("/sign_up")
+def sign_up():
+    return render_template("sign_up.html")
+
+@app.route("/sign_in")
+def sign_in():
+    return render_template("sign_in.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
